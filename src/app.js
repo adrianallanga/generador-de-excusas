8028179fafd48d0c44962a5abc5d9255b8d1b0c5
adrianallanga <rojas-escobar@hotmail.com>
2024-12-07 11:38:11 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whos = who[Math.floor(Math.random() * who.length)];
+  let actions = action[Math.floor(Math.random() * action.length)];
+  let whats = what[Math.floor(Math.random() * what.length)];
+  console.log(whos + " " + actions + " " + whats);
 };
